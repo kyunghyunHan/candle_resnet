e81853ef256bad2d162bae23038ac6f46a19b6e5
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-07-03 22:17:08 +0900

    feat:resnet_update

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,36 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
+use candle_core::{Device, Tensor};
+use candle_nn::linear::Linear;
+use candle_nn::{conv2d, Conv2d, VarBuilder};
+#[derive(Debug)]
 pub struct ResNet {
-    in_channles: u32,
-    conv1: i32,
-    conv2: i32,
-    conv3: i32,
+    cn1: Conv2d,
+    cn2: Conv2d,
+    fc1: Linear,
+    fc2: Linear,
+    fc3: Linear,
 }
 impl ResNet {
-    pub fn new(&mut self) {
-        self.in_channles= 64;
+    pub fn new(device: &Device, vs: VarBuilder) -> Self {
+        let cn1 = conv2d(3, 6, 5, Default::default(), vs.pp("c1")).unwrap();
+        let cn2 = conv2d(6, 16, 5, Default::default(), vs.pp("c2")).unwrap();
+        let fc1 = Linear::new(
+            Tensor::new(&[[16 * 5 * 5i64]], device).unwrap(),
+            Some(Tensor::new(&[84i64], device).unwrap()),
+        );
+        let fc2 = Linear::new(
+            Tensor::new(&[[120i64]], device).unwrap(),
+            Some(Tensor::new(&[84i64], device).unwrap()),
+        );
+        let fc3 = Linear::new(
+            Tensor::new(&[[84i64]], device).unwrap(),
+            Some(Tensor::new(&[10i64], device).unwrap()),
+        );
+        Self {
+            cn1,
+            cn2,
+            fc1,
+            fc2,
+            fc3,
+        }
     }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::ResNet;
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+use candle_core::{DType, Device, Result, Tensor, D};
 use candle_resnet::ResNet;
+
+use candle_nn::{
+    activation, loss, ops, Conv2d, Dropout, Linear, Module, ModuleT, Optimizer, VarBuilder, VarMap,
+};
 struct BasicBlock {}
 impl BasicBlock {
     fn init() {}
@@ -6,5 +11,10 @@ impl BasicBlock {
     fn forawrd() {}
 }
 fn main() {
-    println!("{}", 1);
+    let dev = candle_core::Device::cuda_if_available(0).unwrap();
+
+    let mut varmap = VarMap::new();
+    let vs = VarBuilder::from_varmap(&varmap, DType::F32, &dev);
+   let a=  ResNet::new(&dev, vs);
+    println!("{:?}", a);
 }
